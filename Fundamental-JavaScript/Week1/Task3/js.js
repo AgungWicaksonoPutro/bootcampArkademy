1857b6367e71c2df3fa965ba5712a48d6585e839
AgungWicaksonoPutro <wiputrosnc@gmail.com>
2020-07-19 13:57:46 +0700

    Update Task Weekly

--- Fundamental-JavaScript/Week1/Task3/js.js
+++ Fundamental-JavaScript/Week1/Task3/js.js
@@ -1,8 +1,11 @@
-let n = 5;
-let s = 0;
+let n = prompt("Masukan angka yang akan membentuk pola segitiga: ");
+
+let s = '';
 for (let i = n; i >= 1; i--){
     for (let j = 1; j <= i; j++){
       s += j;
     }
       s += '\n'
   }
+
+  console.log(s);
